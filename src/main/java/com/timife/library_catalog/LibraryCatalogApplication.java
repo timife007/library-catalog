75e6f0a1c9d4a191e32f7e24e64d173ef78d69d1
Timothy Ademola <Timothy@ptml.lan>
2023-11-27 16:27:45 +0100

    initial commit

---
+++ src/main/java/com/timife/library_catalog/LibraryCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.timife.library_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timife/library_catalog/LibraryCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timife.library_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
